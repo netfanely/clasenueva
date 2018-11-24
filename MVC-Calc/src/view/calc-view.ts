0ab08c9ab4c64b29db398540f09a97153cfd3150
Fanny Fanely <40525222+netfanely@users.noreply.github.com>
2018-11-24 14:28:36 -0200

    Add files via upload

--- MVC-Calc/src/view/calc-view.ts
+++ MVC-Calc/src/view/calc-view.ts
@@ -5,9 +5,26 @@ export class CalcView{
     private _input2:number = 0;
 
     Controller : CalcController | undefined;
+   
 
     Equals(a:number, b:number, op:string)
     {
+        if (!this.Controller) { //indefinido
+            return 0;
+        }
         return this.Controller.Equals(a,b,op);
     }
 }
+
+/*
+export class CalView;
+controller: calcController | undefinde;
+initialize
+showInput(value:number){
+    console.log('Input = $(value)`);
+
+}
+simulateSetInput
+
+simulate
+*/
